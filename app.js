2c1368866015b943a7aabbed5500c053afe4140a
erayzeki <erayzekisah@gmail.com>
2022-03-01 16:41:02 +0300

    feat: Add auth route for user register and login

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express();
 // connectDB
 const connectDB = require("./db/connect");
 
+// routers
+const authRouter = require("./routes/authRoute");
+
+// routes
+app.use("/api/v1/auth", authRouter);
+
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
