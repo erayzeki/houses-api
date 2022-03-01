ba38f9139b4e8d6a56ab33b534de19db61c786d5
erayzeki <erayzekisah@gmail.com>
2022-03-01 18:00:55 +0300

    feat: Add controller for user login

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors");
 const User = require("../models/User");
 
 const register = async (req, res) => {
@@ -11,7 +12,31 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send("Login User");
+  const { email, password } = req.body;
+
+  // email and password check
+  if (!email || !password) {
+    throw new BadRequestError("Please provide email and password");
+  }
+
+  // check if user email exist on database
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+
+  // check if password is correct
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid credentials");
+  }
+
+  // return user name and token
+  const token = user.createJWT();
+  res.status(StatusCodes.OK).json({
+    user: { name: user.name },
+    token,
+  });
 };
 
 module.exports = {
