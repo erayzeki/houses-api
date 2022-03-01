b117ecab12d3eca3e17fc66f1cfdcf4500c4f153
erayzeki <erayzekisah@gmail.com>
2022-03-01 21:20:36 +0300

    feat: Add house mongoose model

---
+++ models/House.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const HouseSchema = new mongoose.Schema({
+  town: {
+    type: String,
+    required: [true, "Please provide a town"],
+    minlength: 3,
+    maxlength: 30,
+  },
+  rent: {
+    type: Number,
+    required: [true, "Please provide a rent"],
+    min: 1,
+  },
+  area: {
+    type: Number,
+    required: [true, "Please provide area"],
+    min: 1,
+  },
+  room: {
+    type: String,
+    required: [true, "Please provide the number of rooms in the house"],
+  },
+  bathroom: {
+    type: Number,
+    default: 1,
+    min: 1,
+  },
+  age: {
+    type: String,
+    enum: [
+      "0 (New)",
+      "1",
+      "11-15",
+      "16-20",
+      "2",
+      "21 or more",
+      "3",
+      "4",
+      "5-10",
+    ],
+    required: [true, "Please provide the age of the building"],
+  },
+  balcony: {
+    type: String,
+    enum: ["Yes", "No"],
+    default: "No",
+  },
+  furniture: {
+    type: String,
+    enum: ["Yes", "No"],
+    default: "No",
+  },
+});
+
+module.exports = mongoose.model("House", HouseSchema);
