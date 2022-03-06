3f7e4ff6ea6273c619ed0130bd7cabd571cf3072
ERAY ZEKI SAH <erayzekisah@gmail.com>
2022-03-06 14:43:19 +0300

    feat: Add security packages

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 require("dotenv").config();
 require("express-async-errors");
 
+// security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
 // import and setup express
 const express = require("express");
 const app = express();
@@ -16,7 +22,17 @@ const houseRouter = require("./routes/houseRouter");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 1000 * 60 * 15,
+    max: 100,
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
